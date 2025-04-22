b89c8d6e969710931085b3a34ba5723b0d7742fa
Chebannich <chieban.stas@gmail.com>
2025-04-22 23:35:59 +0200

    feat: Add Projects section with first item

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,40 @@
+import './Projects.scss';
+
+const projects = [
+  {
+    title: 'Christmas Shop',
+    description: 'An online store built with vanilla JavaScript and Sass.',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    image: 'src/assets/img/christmas-shop.jpg',
+    github: 'https://github.com/chebannich/christmas-shop',
+    demo: 'https://chebannich.github.io/christmas-shop/',
+  },
+];
+
 const Projects = () => {
   return (
-    <section className="projects">
-      <h1>My Projects</h1>
+    <section className="projects" id="projects">
+      <div className="container">
+        <h2 className="projects__title">Projects</h2>
+        <div className="projects__grid">
+          {projects.map((project) => (
+            <div key={project.title} className="projects__card">
+              <img src={project.image} alt={project.title} />
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <ul>
+                {project.stack.map((tech) => (
+                  <li key={tech}>{tech}</li>
+                ))}
+              </ul>
+              <div className="projects__links">
+                <a href={project.github} target="_blank" rel="noreferrer">Code</a>
+                <a href={project.demo} target="_blank" rel="noreferrer">Live</a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
