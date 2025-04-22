f05da210ab551d57d7a4949e1c4b696d8401daac
Chebannich <chieban.stas@gmail.com>
2025-04-22 23:51:57 +0200

    feat: Add basic Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 import Contacts from './components/Contacts/Contacts';
-
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -14,6 +14,7 @@ const App = () => {
       <Skills />
       <Projects />
       <Contacts />
+      <Footer />
     </Layout>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="footer__text">© 2025 Stanislav Cheban. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
